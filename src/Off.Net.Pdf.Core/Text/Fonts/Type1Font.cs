33f8e5b9d56ade063cdece80e3dc67abb9c4278b
Victor Pogor <victor.pogor@outlook.com>
2023-03-14 21:05:06 +1000

    feat(font): added basic Type1 font and the standard 14 fonts (#157)
    ### Context
    
    According to the `ISO 32000-1:2008` specification, section `9.6.2`, a `Type 1` font program is a stylized PostScript program that describes glyph shapes. This object is represented as a PDF dictionary.
    
    This dictionary has an entire list of entries but this user story will be focused on supporting only the essential ones for the standard 14 fonts.
    
    ### Required entries
    
    | Key | Type | Constraints |
    | --- | --- | --- |
    | Type | name | Required, must be `Font` |
    | Subtype | name | Required, must be `Type1` |
    | Name | name | Required in PDF1.0 but the validation will be skipped in this story |
    | BaseFont | name | Required, in the context of this story the name must match one of 14 standard fonts |
    
    ### Standard 14 fonts
    
    The PostScript names of 14 Type 1 fonts, known as the standard 14 fonts, are as follows:
    
    - Times-Roman
    - Helvetica
    - Courier
    - Symbol
    - Times-Bold
    - Helvetica-Bold
    - Courier-Bold
    - ZapfDingbats
    - Times-Italic
    - Helvetica-Oblique
    - Courier-Oblique
    - Times-BoldItalic
    - Helvetica-BoldOblique
    - Courier-BoldOblique
    
    ### Acceptance criteria
    
    1. This object must extend the #9
    2. Add the static standard 14 fonts, ~~probably using a Lazy initialization pattern~~
    3. The code must be covered with the unit tests
    
    
    * ci(benchmark): added initial project and a test benchmark
    
    re #156
    
    * feat(font): added basic Type1 font and the standard 14 fonts

---
+++ src/Off.Net.Pdf.Core/Text/Fonts/Type1Font.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using Off.Net.Pdf.Core.Extensions;
+using Off.Net.Pdf.Core.Interfaces;
+using Off.Net.Pdf.Core.Primitives;
+
+namespace Off.Net.Pdf.Core.Text.Fonts;
+
+public sealed class Type1Font : PdfDictionary<IPdfObject>
+{
+    #region Fields
+
+    private static readonly PdfName Type = new("Type");
+    private static readonly PdfName TypeValue = new("Font");
+    private static readonly PdfName Subtype = new("Subtype");
+    private static readonly PdfName SubtypeValue = new("Type1");
+    private static readonly PdfName FontName = new("Name");
+    private static readonly PdfName BaseFont = new("BaseFont");
+
+    #endregion
+
+    #region Constructors
+
+    public Type1Font(Action<Type1FontOptions> optionsFunc) : this(GetType1FontOptions(optionsFunc))
+    {
+    }
+
+    public Type1Font(Type1FontOptions options) : base(GenerateDictionary(options))
+    {
+        options.NotNull(x => x.BaseFont);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private static Type1FontOptions GetType1FontOptions(Action<Type1FontOptions> optionsFunc)
+    {
+        Type1FontOptions fileTrailerOptions = new();
+        optionsFunc.Invoke(fileTrailerOptions);
+        return fileTrailerOptions;
+    }
+
+    private static IReadOnlyDictionary<PdfName, IPdfObject> GenerateDictionary(Type1FontOptions options)
+    {
+        IDictionary<PdfName, IPdfObject> documentCatalogDictionary = new Dictionary<PdfName, IPdfObject>(4)
+            .WithKeyValue(Type, TypeValue)
+            .WithKeyValue(Subtype, SubtypeValue)
+            .WithKeyValue(FontName, options.FontName)
+            .WithKeyValue(BaseFont, options.BaseFont);
+
+        return new ReadOnlyDictionary<PdfName, IPdfObject>(documentCatalogDictionary);
+    }
+
+    #endregion
+}
+
+public sealed class Type1FontOptions
+{
+    public PdfName? FontName { get; set; }
+    public PdfName BaseFont { get; set; } = default!;
+}
