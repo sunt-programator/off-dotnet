7a6f984eb981873ffae1b1b7ca07e4adfd45b226
Victor Pogor <victor.pogor@outlook.com>
2023-12-10 09:23:40 +1000

    feat(syntax): added ArrayElement struct

---
+++ src/OffDotNet.Pdf.CodeAnalysis/Collections/ArrayElement.cs
@@ -0,0 +1,21 @@
+// <copyright file="ArrayElement.cs" company="Sunt Programator">
+// Copyright (c) Sunt Programator. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System.Diagnostics;
+using OffDotNet.Pdf.CodeAnalysis.Syntax.InternalSyntax;
+
+namespace OffDotNet.Pdf.CodeAnalysis.Collections;
+
+[DebuggerDisplay("{Value,nq}")]
+internal struct ArrayElement<TNode>
+    where TNode : GreenNode
+{
+    internal TNode Value;
+
+    public static implicit operator TNode(ArrayElement<TNode> arrayElement)
+    {
+        return arrayElement.Value;
+    }
+}
---
+++ tests/OffDotNet.Pdf.CodeAnalysis.Tests/Collections/ArrayElementTests.cs
@@ -0,0 +1,42 @@
+// <copyright file="ArrayElementTests.cs" company="Sunt Programator">
+// Copyright (c) Sunt Programator. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using OffDotNet.Pdf.CodeAnalysis.Collections;
+using OffDotNet.Pdf.CodeAnalysis.Syntax;
+using OffDotNet.Pdf.CodeAnalysis.Syntax.InternalSyntax;
+using SyntaxToken = OffDotNet.Pdf.CodeAnalysis.Syntax.InternalSyntax.SyntaxToken;
+
+namespace OffDotNet.Pdf.CodeAnalysis.Tests.Collections;
+
+public class ArrayElementTests
+{
+    [Fact(DisplayName = $"The {nameof(ArrayElement<GreenNode>)} struct must have a {nameof(ArrayElement<GreenNode>.Value)} property")]
+    public void ValueProperty()
+    {
+        // Arrange
+        SyntaxToken syntaxToken = SyntaxFactory.Token(SyntaxKind.TrueKeyword);
+        ArrayElement<GreenNode> arrayElement = new() { Value = syntaxToken };
+
+        // Act
+        GreenNode actualValue = arrayElement.Value;
+
+        // Assert
+        Assert.Equal(syntaxToken, actualValue);
+    }
+
+    [Fact(DisplayName = $"The {nameof(ArrayElement<GreenNode>)} struct must have an implicit operator")]
+    public void ImplicitOperator()
+    {
+        // Arrange
+        SyntaxToken syntaxToken = SyntaxFactory.Token(SyntaxKind.TrueKeyword);
+        ArrayElement<GreenNode> arrayElement = new() { Value = syntaxToken };
+
+        // Act
+        GreenNode actualValue = arrayElement;
+
+        // Assert
+        Assert.Equal(syntaxToken, actualValue);
+    }
+}
