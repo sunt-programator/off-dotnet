3cd542043c9c31c194c1e7526fabf73206a78f94
Victor Pogor <victor.pogor@outlook.com>
2024-07-06 13:25:41 +1000

    added more logic in abstract node

--- src/OffDotNet.CodeAnalysis/Syntax/AbstractNode.cs
+++ src/OffDotNet.CodeAnalysis/Syntax/AbstractNode.cs
@@ -5,63 +5,91 @@
 
 namespace OffDotNet.CodeAnalysis.Syntax;
 
+using PooledObjects;
 using Utils;
+using AbstractNodesCacheTuple = (AbstractNode Node, bool Leading, bool Trailing);
 
+/// <summary>Represents the abstract base class for all terminal and non-terminal nodes in the syntax tree.</summary>
 [DebuggerDisplay("{GetDebuggerDisplay(), nq}")]
 internal abstract class AbstractNode
 {
+    /// <summary>Represents a kind of node that does not exist.</summary>
     internal const int NoneKind = 0;
+
+    /// <summary>Represents a kind of node that is a list.</summary>
     internal const int ListKind = 1;
 
+    /// <summary>Indicates that the slot count is too large.</summary>
     protected const int SlotCountTooLarge = 0b0000000000001111;
 
-    private NodeFlagsAndSlotCount _nodeFlagsAndSlotCount;
+    private readonly NodeFlagsAndSlotCount _nodeFlagsAndSlotCount;
 
+    /// <summary>Initializes a new instance of the <see cref="AbstractNode"/> class with the specified raw kind.</summary>
+    /// <param name="rawKind">The raw kind of the node.</param>
     protected AbstractNode(ushort rawKind)
     {
         RawKind = rawKind;
     }
 
+    /// <summary>Initializes a new instance of the <see cref="AbstractNode"/> class with the specified raw kind and full width.</summary>
+    /// <param name="rawKind">The raw kind of the node.</param>
+    /// <param name="fullWidth">The full width of the node.</param>
     protected AbstractNode(ushort rawKind, int fullWidth)
     {
         RawKind = rawKind;
         FullWidth = fullWidth;
     }
 
+    /// <summary>Gets the raw kind of the node.</summary>
     public ushort RawKind { get; }
 
+    /// <summary>Gets the kind text of the node.</summary>
     public abstract string KindText { get; }
 
-    public virtual bool IsToken { get; }
+    /// <summary>Gets a value indicating whether the node is a token.</summary>
+    public virtual bool IsToken => false;
 
+    /// <summary>Gets a value indicating whether the node is trivia.</summary>
     public virtual bool IsTrivia => false;
 
+    /// <summary>Gets a value indicating whether the node is a list.</summary>
     public bool IsList => RawKind == ListKind;
 
+    /// <summary>Gets a value indicating whether the node has leading trivia.</summary>
     public bool HasLeadingTrivia => LeadingTriviaWidth != 0;
 
-    public bool HasTrailingTrivia => LeadingTriviaWidth != 0;
+    /// <summary>Gets a value indicating whether the node has trailing trivia.</summary>
+    public bool HasTrailingTrivia => TrailingTriviaWidth != 0;
 
+    /// <summary>Gets the full width of the node.</summary>
     public int FullWidth { get; }
 
+    /// <summary>Gets the width of the node, excluding leading and trailing trivia.</summary>
     public virtual int Width => FullWidth - LeadingTriviaWidth - TrailingTriviaWidth;
 
+    /// <summary>Gets the value of the node.</summary>
     public virtual Option<object> Value => default;
 
+    /// <summary>Gets the text value of the node.</summary>
     public virtual string ValueText => string.Empty;
 
+    /// <summary>Gets the leading trivia of the node.</summary>
     public virtual Option<AbstractNode> LeadingTrivia => default;
 
+    /// <summary>Gets the trailing trivia of the node.</summary>
     public virtual Option<AbstractNode> TrailingTrivia => default;
 
+    /// <summary>Gets the width of the leading trivia.</summary>
     public virtual int LeadingTriviaWidth => this.FullWidth != 0 && this.GetFirstTerminal().IsSome(out var firstTerminal)
         ? firstTerminal.LeadingTriviaWidth
         : 0;
 
+    /// <summary>Gets the width of the trailing trivia.</summary>
     public virtual int TrailingTriviaWidth => this.FullWidth != 0 && this.GetLastTerminal().IsSome(out var lastTerminal)
         ? lastTerminal.TrailingTriviaWidth
         : 0;
 
+    /// <summary>Gets the number of slots in the node.</summary>
     public int SlotCount
     {
         get
@@ -77,10 +105,9 @@ internal abstract class AbstractNode
         }
     }
 
-    internal abstract Option<AbstractNode> GetSlot(int index);
-
-    protected virtual int GetSlotCount() => 0;
-
+    /// <summary>Gets the offset of the specified slot.</summary>
+    /// <param name="index">The index of the slot.</param>
+    /// <returns>The offset of the slot.</returns>
     public virtual int GetSlotOffset(int index)
     {
         var offset = 0;
@@ -95,6 +122,97 @@ internal abstract class AbstractNode
         return offset;
     }
 
+    /// <summary>Returns a string that represents the current object.</summary>
+    /// <returns>A string that represents the current object.</returns>
+    public override string ToString()
+    {
+        var stringBuilder = SharedObjectPools.StringBuilderPool.Get();
+        using var writer = new StringWriter(stringBuilder, CultureInfo.InvariantCulture);
+
+        WriteTo(writer, leading: false, trailing: false);
+
+        var result = stringBuilder.ToString();
+        SharedObjectPools.StringBuilderPool.Return(stringBuilder);
+        return result;
+    }
+
+    /// <summary>Returns a full string representation of the node, including leading and trailing trivia.</summary>
+    /// <returns>A full string representation of the node.</returns>
+    public virtual string ToFullString()
+    {
+        var stringBuilder = SharedObjectPools.StringBuilderPool.Get();
+        using var writer = new StringWriter(stringBuilder, CultureInfo.InvariantCulture);
+
+        WriteTo(writer, leading: true, trailing: true);
+
+        var result = stringBuilder.ToString();
+        SharedObjectPools.StringBuilderPool.Return(stringBuilder);
+        return result;
+    }
+
+    /// <summary>Gets the slot at the specified index.</summary>
+    /// <param name="index">The index of the slot.</param>
+    /// <returns>The slot at the specified index.</returns>
+    internal abstract Option<AbstractNode> GetSlot(int index);
+
+    /// <summary>Gets the number of slots in the node.</summary>
+    /// <returns>The number of slots.</returns>
+    protected virtual int GetSlotCount() => 0;
+
+    /// <summary>Writes the trivia of the node to the specified writer.</summary>
+    /// <param name="writer">The writer to which the trivia will be written.</param>
+    protected virtual void WriteTriviaTo(TextWriter writer)
+    {
+        Debug.Fail("Should not get here.");
+    }
+
+    /// <summary>Writes the token of the node to the specified writer.</summary>
+    /// <param name="writer">The writer to which the token will be written.</param>
+    /// <param name="leading">Indicates whether to include leading trivia.</param>
+    /// <param name="trailing">Indicates whether to include trailing trivia.</param>
+    protected virtual void WriteTokenTo(TextWriter writer, bool leading, bool trailing)
+    {
+        Debug.Fail("Should not get here.");
+    }
+
+    /// <summary>Writes the node to the specified writer.</summary>
+    /// <param name="writer">The writer to which the node will be written.</param>
+    /// <param name="leading">Indicates whether to include leading trivia.</param>
+    /// <param name="trailing">Indicates whether to include trailing trivia.</param>
+    [SuppressMessage("Minor Code Smell", "S3626:Jump statements should not be redundant", Justification = "False positive.")]
+    protected void WriteTo(TextWriter writer, bool leading, bool trailing)
+    {
+        var stack = SharedObjectPools.AbstractNodesCache.Get();
+        stack.Push((this, leading, trailing));
+
+        ProcessStack(writer, stack);
+        SharedObjectPools.AbstractNodesCache.Return(stack);
+        return;
+
+        static void ProcessStack(TextWriter writer, Stack<AbstractNodesCacheTuple> stack)
+        {
+            while (stack.Count > 0)
+            {
+                var currentTuple = stack.Pop();
+
+                if (currentTuple.Node.IsTrivia)
+                {
+                    currentTuple.Node.WriteTriviaTo(writer);
+                    continue;
+                }
+
+                if (currentTuple.Node.IsToken)
+                {
+                    currentTuple.Node.WriteTokenTo(writer, currentTuple.Leading, currentTuple.Trailing);
+                }
+
+                // TODO: handle nested nodes
+            }
+        }
+    }
+
+    /// <summary>Gets the first terminal node in the tree.</summary>
+    /// <returns>The first terminal node.</returns>
     private Option<AbstractNode> GetFirstTerminal()
     {
         Option<AbstractNode> node = this;
@@ -120,6 +238,8 @@ internal abstract class AbstractNode
         return node;
     }
 
+    /// <summary>Gets the last terminal node in the tree.</summary>
+    /// <returns>The last terminal node.</returns>
     private Option<AbstractNode> GetLastTerminal()
     {
         Option<AbstractNode> node = this;
@@ -145,6 +265,8 @@ internal abstract class AbstractNode
         return node;
     }
 
+    /// <summary>Gets the debugger display string for the node.</summary>
+    /// <returns>The debugger display string.</returns>
     private string GetDebuggerDisplay()
     {
         return this.GetType().Name + " " + this.KindText + " " + this;
@@ -159,6 +281,9 @@ internal abstract class AbstractNode
 
         private ushort _data;
 
+        /// <summary>
+        /// Gets or sets the small slot count.
+        /// </summary>
         public byte SmallSlotCount
         {
             readonly get
