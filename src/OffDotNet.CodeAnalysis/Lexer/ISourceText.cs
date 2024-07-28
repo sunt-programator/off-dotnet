f548f487c5da059d853d87c62c95006f09d2f1b1
Victor Pogor <victor.pogor@outlook.com>
2024-07-28 13:12:51 +1000

    added source text

---
+++ src/OffDotNet.CodeAnalysis/Lexer/ISourceText.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="ISourceText.cs" company="Sunt Programator">
+// Copyright (c) Sunt Programator. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace OffDotNet.CodeAnalysis.Lexer;
+
+public interface ISourceText
+{
+    ReadOnlyMemory<byte> Source { get; }
+
+    int Length { get; }
+
+    byte this[int position] { get; }
+
+    void CopyTo(int sourceIndex, byte[] destination, int destinationIndex, int count);
+}
---
+++ src/OffDotNet.CodeAnalysis/Lexer/StringText.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="StringText.cs" company="Sunt Programator">
+// Copyright (c) Sunt Programator. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace OffDotNet.CodeAnalysis.Lexer;
+
+internal sealed class StringText : ISourceText
+{
+    internal StringText(in ReadOnlySpan<byte> source)
+    {
+        Source = source.ToArray();
+    }
+
+    public ReadOnlyMemory<byte> Source { get; }
+
+    public int Length => Source.Length;
+
+    public byte this[int position] => Source.Span[position];
+
+    public void CopyTo(int sourceIndex, byte[] destination, int destinationIndex, int count)
+    {
+        Source.Span.Slice(sourceIndex, count).CopyTo(destination.AsSpan(destinationIndex));
+    }
+}
